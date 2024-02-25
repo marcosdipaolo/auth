c656144e62cfd82ecb63657c272fe1a4a6d9cf9a
Marcos Di Paolo <marcosdipaolo@gmail.com>
2024-02-25 02:39:26 -0300

    Authenticatable with private props

--- src/Authenticatable.php
+++ src/Authenticatable.php
@@ -4,8 +4,49 @@ namespace MDP\Auth;
 
 abstract class Authenticatable
 {
-    public int | string $id;
-    public string $username;
-    public string $email;
-    public string $password;
+    private int | string $id;
+    private string $username;
+    private string $email;
+    private string $password;
+
+    public function getId(): int|string
+    {
+        return $this->id;
+    }
+
+    public function setId(int|string $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
 }
